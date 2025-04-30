a4c2ef8f2ed38fab9b0e05e567b14d2bbef962ac
Ksusha <https://github.com/ksupiska>
2025-04-30 20:14:26 +0300

    добавление загрузки перед начальным входом на сайт

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // src/App.tsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import Home from './pages/Home';
 import SimCreateForm from './pages/SimCreateForm';
@@ -7,10 +8,26 @@ import SimCreateForm from './pages/SimCreateForm';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CharacterList from './components/CharacterList';
+import LoadingComponent from './components/LoadingComponent';
 
+import './index.css';
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  // Эмуляция загрузки контента
+  useEffect(() => {
+    // Обычно здесь будет запрос к серверу или другая асинхронная логика
+    const timer = setTimeout(() => {
+      setLoading(false); // Убираем индикатор загрузки через 3 секунды
+    }, 3000);
+
+    // Очистка таймера, если компонент размонтирован
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Router>
+      {loading && <LoadingComponent />} {/* Показываем индикатор загрузки, если данные еще не загружены */}
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/components/LoadingComponent.tsx
@@ -0,0 +1,11 @@
+// src/components/LoadingComponent.tsx
+const LoadingComponent = () => {
+    return (
+      <div className="loading-container">
+        <div className="loading-spinner"></div>
+      </div>
+    );
+  };
+  
+  export default LoadingComponent;
+  
